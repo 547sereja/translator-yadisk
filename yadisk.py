68a73872208e7674fbe24d5f9ed6f746445a15aa
Sergey Illyushko <serhiiilliushko@gmail.com>
2020-06-11 22:44:18 +0300

    third commit

--- yadisk.py
+++ yadisk.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 params = {"path": "123.text"}
-headers = {'Authizatio': 'OAuth AgAAAAAKV4gXAAZkw3TOttBuzkMPkC8wqmVrkUA'}
+headers = {'Authizatio': 'OAuth AgAAAAAKV4gw3TOttBuzkMPkC8wqmVrkUA'}
 
 response = requests.put(url, headers=headers, params=params)
 
