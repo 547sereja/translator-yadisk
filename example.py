76211e21ea2104cd56ca051ccfa81a2a03617c6b
Sergey Illyushko <serhiiilliushko@gmail.com>
2020-06-13 18:28:18 +0300

    fixed translator

--- example.py
+++ example.py
@@ -1,37 +1,62 @@
 import requests
-#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
-API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
-
-    params = {
-        'key': API_KEY,
-        'text': text,
-        'lang': 'ru'.format(to_lang),
-    }
-
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-    return ''.join(json_['text'])
+API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 
 
-def main(file, lang):
-    with open(input("enter how to save"), 'w', encoding='utf-8') as f:
-        f.write(translate_it(file, lang))
+def translate_me(open_file, save_file,  language):
+    with open(open_file, encoding='utf-8') as file_text:
+        params = {
+            'key': API_KEY,
+            'text': file_text,
+            'lang': 'ru'.format(language),
+        }
+
+        response = requests.get(URL, params=params)
+        json_ = response.json()
+        to_write = ''.join(json_['text'])
+    with open(save_file, 'w', encoding='utf-8') as saved_file:
+        saved_file.write(to_write)
 
 
-if __name__ == '__main__':
-    main(open(input("enter your file to open"), encoding='utf-8'), 'ru')
+translate_me('DE.txt', 'translated.txt', 'ru')
+
+# def open_file(file):
+#     with open(file, encoding='utf-8') as text:
+#         return text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     def save_name(file):
+#         with open(input("Enter file name to save"), 'w', encoding='utf-8') as f:
+#             f.write(translated)
+#
+#
+#
+# # def save_name(file):
+# #     with open(input("Enter file name to save"), 'w', encoding='utf-8') as f:
+# #         f.write(translate_it(file))
+#
+#
+# def open_file():
+#     with open(input("Enter file name to open"), encoding='utf-8') as text:
+#         return text
+
+
+
+
+# if __name__ == '__main__':
+#     main(open(input("enter your file to open"), encoding='utf-8'), 'ru')
--- yadisk.py
+++ yadisk.py
@@ -3,29 +3,15 @@ from pprint import pprint
 
 url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 params = {"path": "123.text"}
-headers = {'Authizatio': 'OAuth AgAAAAAKV4gw3TOttBuzkMPkC8wqmVrkUA'}
+headers = {"Authorization:OAuth oauth_token=token, oauth_client_id=id"}
+
+
+id = "ID: 792e5dcaf75a454f9dc7f53f653dd3ba"
+Password = "0c34f27992aa459fbc013aeaa"
+Callback_URL = "https://oauth.yandex.ru/verification_code"
+
+token = "AgAAAAAKV4gXAAZkw3TOttBuzkMPkC8"
 
 response = requests.put(url, headers=headers, params=params)
 
 pprint(response.json())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- # ? path=<путь, по которому следует загрузить файл>
- # & [overwrite=<признак перезаписи>]
- # & [fields=<свойства, которые нужно включить в ответ>]
